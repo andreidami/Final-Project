1c600c70e496d11de3266c2d36c2d55d442380d2
Damian Andrei <andrei_damian27@yahoo.com>
2020-10-22 11:17:11 +0300

    add Prototipe

---
+++ PhotoApp/photoapp/src/Portofolio.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+import React from "react";
+// import DogList from "./DogList";
+// import { dogs } from "./dogs";
+// import SearchBox from "./SearchBox";
+// import {link} from 'react-router-dom'
+import Photos from "./Photos"
+
+
+
+// export const Portofolio = (props) => {
+// return(
+
+    function Portofolio() {
+      return (
+        <div>
+          <div>
+            <h1 className="Portofolio-title">Portofolio</h1>
+            <Photos/>
+          </div>
+        </div>
+      );
+    }
+   
+    export default Portofolio;
+                                          
